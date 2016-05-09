98f03dc5170e6e3ffcaa0189772176343a5cf5ce
Marcel Stassen <Marcel_Stassen@hotmail.com>
2016-05-09 12:57:38 +0200

    basic  calc build

--- KassaBlijDorp/KassaBlijDorp/Program.cs
+++ KassaBlijDorp/KassaBlijDorp/Program.cs
@@ -10,7 +10,37 @@ namespace KassaBlijDorp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World");
+      string volwassenen;
+      string kinderen;
+      Console.WriteLine("Hoeveel volwassenen");
+      volwassenen = Console.ReadLine();
+      Console.WriteLine("Hoeveel kinderen");
+      kinderen = Console.ReadLine();
+      bereken test = new bereken();
+      Console.WriteLine(test.bereken1(volwassenen, kinderen));
+
+
+
+    }
+  }
+  class bereken
+  {
+    public string bereken1(string v , string k)
+    {
+      int totaal=0;
+      int Iv = Convert.ToInt32(v);
+      int Ik = Convert.ToInt32(k);
+
+      if (Iv != 0)
+      {
+        totaal += (Iv * 20);
+      }
+      if (Ik != 0)
+      {
+        totaal +=( Ik * 17);
+      }
+      string EindTotaal = totaal.ToString();
+      return EindTotaal;
     }
   }
 }
