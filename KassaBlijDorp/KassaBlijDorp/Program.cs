847fdc0608684cb278432dfe8995cee3029c3227
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-05-09 19:29:19 +0200

    Abonnementen + introducé-korting toegevoegd

--- KassaBlijDorp/KassaBlijDorp/Program.cs
+++ KassaBlijDorp/KassaBlijDorp/Program.cs
@@ -12,12 +12,22 @@ namespace KassaBlijDorp
     {
       string volwassenen;
       string kinderen;
-      Console.WriteLine("Hoeveel volwassenen");
+      string begeleider;
+      string abonnement;
+      Console.WriteLine("Aantal volwassenen (vanaf 13 jaar, zonder abonnement):");
       volwassenen = Console.ReadLine();
-      Console.WriteLine("Hoeveel kinderen");
+
+      Console.WriteLine("Aantal kinderen (3 t/m 12 jaar, zonder abonnement):");
       kinderen = Console.ReadLine();
+
+      Console.WriteLine("Aantal begeleiders van gehandicapte personen (zonder abonnement):");
+      begeleider = Console.ReadLine();
+
+      Console.WriteLine("Aantal abonnementhouders:");
+      abonnement = Console.ReadLine();
+
       bereken test = new bereken();
-      Console.WriteLine(test.bereken1(volwassenen, kinderen));
+      Console.WriteLine(test.bereken1(volwassenen, kinderen, begeleider, abonnement));
       Console.ReadLine();
 
 
@@ -25,13 +35,19 @@ namespace KassaBlijDorp
   }
   class bereken
   {
-    public string bereken1(string v , string k)
+    public string bereken1(string v , string k, string b, string a)
     {
       double totaal = 0;
       int Iv = Convert.ToInt32(v);
       int Ik = Convert.ToInt32(k);
+      int Ib = Convert.ToInt32(b);
+      int Ia = Convert.ToInt32(a);
       double vP = 22.00;
       double kP = 17.50;
+      double bP = 12.00;
+      double disc = 0.25;
+
+      int Intr = Ia * 4;
 
       if (Ik+ Iv >= 20)
       {
@@ -43,10 +59,47 @@ namespace KassaBlijDorp
       {
         totaal += (Iv * vP);
       }
+
       if (Ik > 0)
       {
         totaal +=( Ik * kP);
       }
+
+      if (Ib > 0)
+      {
+        totaal += (Ib * bP);
+      }
+
+      if (Intr >= Iv)
+      {
+        totaal -= (disc * vP) * Iv;
+        Intr -= Iv;
+
+        if (Intr >= Ik)
+        {
+          totaal -= (disc * kP) * Ik;
+          Intr -= Ik;
+
+          if(Intr >= Ib)
+          {
+            totaal -= (disc * bP) * Ib;
+          }
+          else
+          {
+            totaal -= (disc * bP) * Intr;
+          }
+        }
+        else
+        {
+          totaal -= (disc * kP) * Intr;
+        }
+      }
+      else
+      {
+        totaal -= (disc * vP) * Intr;
+      }
+
+
       string EindTotaal = totaal.ToString();
       return EindTotaal;
     }
