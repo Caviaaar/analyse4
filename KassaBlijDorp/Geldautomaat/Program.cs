0ec474da6f2d5c14ad2f4ce4d1f29afbf33f7ed5
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-05-09 22:06:25 +0200

    Geldautomaat geschreven. Typechecking moet nog worden toegevoegd.

--- KassaBlijDorp/Geldautomaat/Program.cs
+++ KassaBlijDorp/Geldautomaat/Program.cs
@@ -11,10 +11,9 @@ namespace Geldautomaat
     static void Main(string[] args)
     {
       Automaat A = new Automaat();
+      Console.WriteLine(A.Opnemen());
       Console.ReadLine();
-    }
-    
-    
+    }    
   }
 
   class Automaat
@@ -24,6 +23,7 @@ namespace Geldautomaat
     public double Krediet;
     public double Voorraad;
     public double Opname;
+    public bool Actief;
 
     public Automaat()
     {
@@ -32,12 +32,56 @@ namespace Geldautomaat
       this.Krediet = Input("Voer uw kredietlimiet in: ");
       this.Voorraad = Input("Voer het beschikbare bedrag in de geldautomaat in: ");
       this.Opname = Input("Voer het bedrag in wat u op wilt nemen: ");
+
+      Console.WriteLine("Is de automaat in werking (Y/N): ");
+      string werkend = Console.ReadLine();
+      if(werkend == "Y" || werkend == "y")
+      {
+        this.Actief = true;
+      }
+      else
+      {
+        this.Actief = false;
+      }
     }
 
     public double Input(string text)
     {
       Console.WriteLine(text);
       return Convert.ToDouble(Console.ReadLine());
+    }
+
+    public string Opnemen()
+    {
+      if(Actief)
+      {
+        if(Saldo + Krediet >= Opname)
+        {
+          if(Opname <= Voorraad)
+          {
+            if(Opname <= Limiet)
+            {
+              return "De transactie is geslaagd!";
+            }
+            else
+            {
+              return "De transactie is mislukt. U overschrijdt uw dagmlimiet!";
+            }
+          }
+          else
+          {
+            return "De transactie is mislukt. Er is niet voldoende geld voorradig";
+          }
+        }
+        else
+        {
+          return "De transactie is mislukt. U heeft onvoldoende saldo en krediet om het gewenste bedrag op te nemen!";
+        }
+      }
+      else
+      {
+        return "De automaat is niet in werking dus er kan geen geld worden opgenomen!";
+      }
     }
   }
 }
