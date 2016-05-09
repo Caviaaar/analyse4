b698ff47bdb93d477f2b2beb599d71979cc04143
Jelle Kerkstra <jellekerkstra@hotmail.com>
2016-05-09 21:53:15 +0200

    Inputs toegevoegd

--- KassaBlijDorp/Geldautomaat/Program.cs
+++ KassaBlijDorp/Geldautomaat/Program.cs
@@ -10,6 +10,34 @@ namespace Geldautomaat
   {
     static void Main(string[] args)
     {
+      Automaat A = new Automaat();
+      Console.ReadLine();
+    }
+    
+    
+  }
+
+  class Automaat
+  {
+    public double Saldo;
+    public double Limiet;
+    public double Krediet;
+    public double Voorraad;
+    public double Opname;
+
+    public Automaat()
+    {
+      this.Saldo = Input("Voer uw banksaldo in: ");
+      this.Limiet = Input("Voer uw daglimiet in: ");
+      this.Krediet = Input("Voer uw kredietlimiet in: ");
+      this.Voorraad = Input("Voer het beschikbare bedrag in de geldautomaat in: ");
+      this.Opname = Input("Voer het bedrag in wat u op wilt nemen: ");
+    }
+
+    public double Input(string text)
+    {
+      Console.WriteLine(text);
+      return Convert.ToDouble(Console.ReadLine());
     }
   }
 }
